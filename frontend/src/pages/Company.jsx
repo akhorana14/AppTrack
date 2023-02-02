9198ac728a00cc20bd09590616acec27c76bc9f8
IshaanRaja <ishaanrajapurdue@gmail.com>
2023-02-02 14:51:23 -0500

    Add basic project structure + install bootstrap

---
+++ frontend/src/pages/Company.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header"
+import Navbar from "../components/Navbar"
+
+function Company() {
+    return (
+        <>
+            <Header title="Company Page" />
+            <Navbar />
+            <p>Hello, World!</p>
+        </>
+    );
+};
+
+export default Company;
