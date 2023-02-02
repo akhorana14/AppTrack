566ea4c50be16c28f1677747d40f4c88ffd76f6e
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2023-02-01 22:10:22 -0500

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar/Navbar';
+import './Navbar/Navbar.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ frontend/src/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+        <h1>AppTrack</h1>
+        </div>
+    );
+}
+export default Navbar;
