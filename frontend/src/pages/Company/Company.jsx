c56106a33c9b0b6621950c96aa5d63e5640591b3
IshaanRaja <ishaanrajapurdue@gmail.com>
2023-02-07 22:24:35 -0500

    Add action items and email history

--- frontend/src/pages/Company/Company.jsx
+++ frontend/src/pages/Company/Company.jsx
@@ -8,8 +8,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
 
-import { CheckCircleFill, ExclamationCircleFill } from 'react-bootstrap-icons';
+import {BellFill, CheckCircleFill, EnvelopeFill, ExclamationCircleFill} from 'react-bootstrap-icons';
 
 //Replace this later with a call to the backend API
 let listOfStages = [
@@ -17,6 +18,12 @@ let listOfStages = [
     {date: "01/02/2022", type: "Online Assessment"},
     {date: "01/03/2022", type: "Interview 1"}
 ];
+
+let emails = [
+    {subject: "Thank You For Applying!", body: "Applied"},
+    {subject: "Please complete this Leetcode Assessment.", body: "Online Assessment"},
+    {subject: "You've been invited to Interview", body: "Interview 1"}
+]
 
 function StageList(props) {
     return (
@@ -33,6 +40,7 @@ function StageList(props) {
                                 {stage.type}
                                 <span className="text-muted"> {stage.date}</span>
                             </h6>
+                            <div className="vertical-line"></div>
                         </div>
                     </li>
                     );
@@ -41,12 +49,53 @@ function StageList(props) {
         </ul>
     )
 }
+
 function LevelsButton() {
     return (
-        <Button variant="light" className="d-flex align-items-center levels-btn">
+        <Button variant="light" className="levels-btn d-flex align-items-center">
            <img src="https://www.levels.fyi/assets/levelsiconfilledcolored.png" height="28" width="28" alt="Levels.fyi Logo" />
            Levels.fyi
         </Button>
+    );
+}
+
+function LeetcodeButton() {
+    return (
+        <Button variant="dark" className="leetcode-btn d-flex align-items-center">
+           <img src="https://leetcode.com/static/images/LeetCode_logo_rvs.png" height="28" width="28" alt="Leetcode Logo" />
+           Leetcode
+        </Button>
+    );
+}
+
+function EmailAccordion(props) {
+    return (
+         <Accordion flush className="mini-accordion">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>
+                    <h6 className="m-0">{props.subject}</h6>
+                </Accordion.Header>
+                <Accordion.Body>
+                  {props.body}
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+    );
+}
+
+function ActionItemAccordion(props) {
+    return (
+         <Accordion flush className="mini-accordion">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>
+                    <h6 className="m-0">{props.subject}</h6>
+                    <button type="button" class="btn-close btn-sm position-absolute end-0 me-5" aria-label="Close"></button>
+                </Accordion.Header>
+                <Accordion.Body>
+                  {props.body}
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
     );
 }
 
@@ -59,18 +108,47 @@ function Company() {
             <Navbar />
             <div className="main-content">
                 <Container fluid className="h-100">
-                    <Row className="main-row justify-content-center h-100">
+                    <Row className="justify-content-center h-100">
                         <Col className="left left-half">
-                            <h1 className="display-3 font-weight-normal">{`${companyName}`}</h1>
+                            <h1 className="display-3 font-weight-normal mb-0">{`${companyName}`}</h1>
                             <h3>Software Engineering Intern
                                 <br />
                                 <h6 className="text-muted">Last update: January 23, 2022</h6>
                             </h3>
-                            <LevelsButton />
+                            <div className="mt-1 d-flex align-items-center">
+                                <LevelsButton />
+                                <LeetcodeButton />
+                            </div>
                            <StageList list={listOfStages} />
                         </Col>
                         <Col className="right right-half">
-                        Hi
+                            <div className="d-flex align-items-center pb-2">
+                                <BellFill size={24}/>
+                                <h4 className="ps-1 m-0">Action Items</h4>
+                            </div>
+
+                            <Row className="action-items">
+                                <Col className="mw-100 p-2 m-1 shadow-sm">
+                                    <ActionItemAccordion subject="Upcoming Interview" body="test"/>
+                                </Col>
+                            </Row>
+
+                            <div className="d-flex align-items-center pb-2 mt-5">
+                                        <EnvelopeFill size={24}/>
+                                        <h4 className="ps-1 m-0">Email History</h4>
+                            </div>
+
+                            <Row className="email-history">
+                                <Col className="mw-100 p-2 m-1 shadow-sm">
+                                    {
+                                        emails.slice(0).reverse().map(email  => {
+                                            return (
+                                                <EmailAccordion {...email} />
+                                            )
+                                        })
+                                    }
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
                 </Container>
