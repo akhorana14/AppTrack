de51efa0f6badae14aa08ebe62468cb418f02815
rayyan-khan <rayyan.khan258@gmail.com>
2023-02-10 00:12:57 -0500

    added route

--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,12 @@
-import Header from "../../components/Header"
 import Navbar from "../../components/Navbar/Navbar"
-import './Company.css';
-
-import {useParams} from "react-router-dom";
 
 function Dashboard() {
-    
+    return (
+        <>
+            <Navbar />
+            <p>Dashboard page</p>
+        </>
+    );
 };
 
 export default Dashboard;
