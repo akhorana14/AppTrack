71cc88953cd8a104f925d907ba11c1041ecccc3d
rayyan-khan <rayyan.khan258@gmail.com>
2023-02-09 23:59:14 -0500

    initial commit

---
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header"
+import Navbar from "../../components/Navbar/Navbar"
+import './Company.css';
+
+import {useParams} from "react-router-dom";
+
+function Dashboard() {
+    
+};
+
+export default Dashboard;
