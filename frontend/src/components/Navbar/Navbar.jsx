d99c8fda07a9b1cb167d6d34bc059725107fce28
Rayyan Khan <41585552+rayyan-khan@users.noreply.github.com>
2023-02-09 23:50:24 -0500

    Revert "Navbar2"

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,42 +1,34 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import * as Icon from 'react-bootstrap-icons';
+import Container from 'react-bootstrap/Container';
 
 import './Navbar.css';
 
 function getNavbar() {
-    const navDropDownTitle = (<Icon.Justify href="#menu" class="menu">Menu</Icon.Justify>)
-    const navHomeButton = (<Icon.HouseDoorFill href="#home" class="home">Home</Icon.HouseDoorFill>)
-    const navProfileButton = (<Icon.PersonFill href="#profile" class="profile">Profile</Icon.PersonFill>)
     return (
         <Navbar variant="dark" expand="lg" >
-          <Navbar.Brand href="#home">AppTrack</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-
-            <Nav className="ml-auto"> {/* align below items to the right */}
-            <NavDropdown title={navDropDownTitle} id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">
-                  Dashboard</NavDropdown.Item>
-
-                <NavDropdown.Item href="#action/3.2">
-                  Calendar</NavDropdown.Item>
-
-                <NavDropdown.Item href="#action/3.3">
-                  Companies</NavDropdown.Item>
-
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Settings
-                </NavDropdown.Item>
-                
-              </NavDropdown>
-              
-              <button type="button" class="btn btn-link">{navProfileButton}</button>
-              <button type="button" class="btn btn-link">{navHomeButton}</button>
-            </Nav>
-          </Navbar.Collapse>
+          <Container fluid>
+            <Navbar.Brand href="#home">AppTrack</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
       </Navbar>
     );
 }
