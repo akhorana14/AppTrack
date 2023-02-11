58d1d1f815fbac383b7e97216a2044ef1d13d763
unknown <dusi.abhinav2002@gmail.com>
2023-02-11 15:12:53 -0500

    Upcoming and completed events

--- frontend/src/pages/Calendar/Calendar.jsx
+++ frontend/src/pages/Calendar/Calendar.jsx
@@ -5,14 +5,6 @@ import Navbar from "../../components/Navbar/Navbar";
 import CalendarUtil from "./CalendarUtil";
 import Event from "./Components/Event/Event";
 
-// TODO
-// Lists contain bootstrap icons/style
-// Swap through ~3 pages of completed/upcoming lists (transition animations)
-// Display color-coded events in calendar days
-// Calendar days must be scrollable
-// Simulate retrieved events with hardcoded objects
-// Call API to retrieve events
-
 function Calendar(props) {
     const eventsPerPage = 4;
 
@@ -28,20 +20,36 @@ function Calendar(props) {
     const [numUpcomingPages, setNumUpcomingPages] = useState(3); 
     const [numCompletedPages, setNumCompletedPages] = useState(3); 
 
+    const [calendarEvents, setCalendarEvents] = useState([]);
+
     useEffect(() => {
         var retrievedUpcomingEvents = []; 
         for (var i = 0; i < 15; i++) {
             retrievedUpcomingEvents.push(
                 {
-                    company: "Amazon",
+                    company: "Amazon" + i,
                     date: "2/9/2023",
-                    status: "Interview"
+                    subject: "Interview",
+                    companyURL: "company/" + i
                 }
             )
         }
         setUpcomingEvents(retrievedUpcomingEvents);
-        setNumUpcomingPages(Math.min(3, Math.floor(retrievedUpcomingEvents.length/eventsPerPage)+1)); 
+        setNumUpcomingPages(Math.min(3, Math.floor((retrievedUpcomingEvents.length-1)/eventsPerPage)+1)); 
 
+        var retrievedCompletedEvents = []; 
+        for (i = 0; i < 4; i++) {
+            retrievedCompletedEvents.push(
+                {
+                    company: "Google" + i,
+                    date: "2/11/2023",
+                    subject: "Accepted",
+                    companyURL: "company/" + i
+                }
+            )
+        }
+        setCompletedEvents(retrievedCompletedEvents);
+        setNumCompletedPages(Math.min(3, Math.floor((retrievedCompletedEvents.length-1)/eventsPerPage)+1));
     }, []);
 
     function handleUpcomingPageChange(increment) {
@@ -54,8 +62,6 @@ function Calendar(props) {
                 setUpcomingPage(upcomingPage - 1);
             }
         }
-
-        console.log(upcomingPage); 
     }
 
     function handleCompletedPageChange(increment) {
@@ -68,27 +74,43 @@ function Calendar(props) {
                 setCompletedPage(completedPage - 1);
             }
         }
-
-        console.log(completedPage); 
     }
 
     function jsxUpcomingEvents() {
-        var events = [];
+        var events = upcomingEvents.slice(4*upcomingPage-4, 4*upcomingPage);
         return (
             <>
-                <Event />
-                <Event />
-                <Event />
+                {
+                    events.map((event, key) => {
+                        return <Event key={key} 
+                            company={event.company} 
+                            date={event.date} 
+                            subject={event.subject} 
+                            upcoming={true}
+                            companyURL={event.companyURL}
+                        />
+                    })
+                }
             </>
         );
     }
 
     function jsxCompletedEvents() {
+        var events = completedEvents.slice(4*completedPage-4, 4*completedPage);
         return (
             <>
-                <Event />
-                <Event />
-                <Event />
+                {
+                    events.map((event, key) => {
+                        return <Event 
+                            key={key} 
+                            company={event.company} 
+                            date={event.date} 
+                            subject={event.subject} 
+                            upcoming={false}
+                            companyURL={event.companyURL}
+                        />
+                    })
+                }
             </>
         );
     }
@@ -109,15 +131,24 @@ function Calendar(props) {
     function jsxWeekDays(week) {
         var weekDays = [];
         for (var day = 0; day < 7; day++) {
-            var className = "calendar-box";
-            if (week !== 5) className += " border-bottom";
-            if (day !== 0) className += " border-left";
-            if (calendar[week][day].isToday) className += " current-day";
-            if (!calendar[week][day].isDisplayedMonth) className += " not-current-month";
+            var boxClassName = "calendar-box";
+            if (week !== 5) boxClassName += " border-bottom";
+            if (day !== 0) boxClassName += " border-left";
+            if (!calendar[week][day].isDisplayedMonth) boxClassName += " not-current-month";
+
+            var dateClassName = "";
+            if (calendar[week][day].isToday) dateClassName += " current-day";
 
             weekDays.push(
-                <div className={className} key={day}>
-                    {calendar[week][day].date}
+                <div className={boxClassName} key={day}>
+                    <div className={dateClassName}>
+                        {calendar[week][day].date}
+                    </div>
+                    <ul>
+                        <li style={{color: "red"}}><span style={{color: "black"}}>Event 1</span></li>
+                        <li style={{color: "green"}}><span style={{color: "black"}}>Event 2</span></li>
+                        <li style={{color: "blue"}}><span style={{color: "black"}}>Event 3</span></li>
+                    </ul>
                 </div>
             );
         }
@@ -145,24 +176,56 @@ function Calendar(props) {
                         <span className="lead left-headers">Upcoming</span>
                         {jsxUpcomingEvents()}
                         <div className="bottom-arrows-container">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-left-circle page-arrows" viewBox="0 0 16 16" onClick={() => handleUpcomingPageChange(false)}>
-                                <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-right-circle page-arrows" viewBox="0 0 16 16" onClick={() => handleUpcomingPageChange(true)}>
-                                <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-                            </svg>
+                            {
+                                upcomingPage === 1 
+                                ? 
+                                <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-left-circle page-arrows" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                                </svg>
+                                :
+                                <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-left-circle page-arrows page-arrows-enabled" viewBox="0 0 16 16"  onClick={() => handleUpcomingPageChange(false)}>
+                                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                                </svg>
+                            }
+                            {
+                                upcomingPage === numUpcomingPages
+                                ?
+                                <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-right-circle page-arrows" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                                </svg>
+                                :
+                                <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-right-circle page-arrows page-arrows-enabled" viewBox="0 0 16 16"  onClick={() => handleUpcomingPageChange(true)}>
+                                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                                </svg>
+                            }
                         </div>
                     </div>
                     <div className="left-bottom">
                         <span className="lead left-headers">Completed</span>
                         {jsxCompletedEvents()}
                         <div className="bottom-arrows-container">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-left-circle page-arrows" viewBox="0 0 16 16" onClick={() => handleCompletedPageChange(false)}>
-                                <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-right-circle page-arrows" viewBox="0 0 16 16" onClick={() => handleCompletedPageChange(true)}>
-                                <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-                            </svg>
+                            {
+                                completedPage === 1 
+                                ? 
+                                <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-left-circle page-arrows" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                                </svg>
+                                :
+                                <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-left-circle page-arrows page-arrows-enabled" viewBox="0 0 16 16"  onClick={() => handleCompletedPageChange(false)}>
+                                    <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                                </svg>
+                            }
+                            {
+                                completedPage === numCompletedPages
+                                ?
+                                <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-right-circle page-arrows" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                                </svg>
+                                :
+                                <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-right-circle page-arrows page-arrows-enabled" viewBox="0 0 16 16"  onClick={() => handleCompletedPageChange(true)}>
+                                    <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                                </svg>
+                            }
                         </div>
                     </div>
                 </div>
