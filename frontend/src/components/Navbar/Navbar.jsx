b0829459ecc4d0a5dd086b8919f39f0b06dfadef
IshaanRaja <ishaanrajapurdue@gmail.com>
2023-02-12 16:03:49 -0500

    Correct navbar alignment issue + move dashboard text-center property to prevent other page conflicts

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,43 +1,47 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import * as Icon from 'react-bootstrap-icons';
 
 import './Navbar.css';
 
 function getNavbar() {
-    const navDropDownTitle = (<Icon.Justify href="#menu" class="menu">Menu</Icon.Justify>)
-    const navHomeButton = (<Icon.HouseDoorFill href="#home" class="home">Home</Icon.HouseDoorFill>)
-    const navProfileButton = (<Icon.PersonFill href="#profile" class="profile">Profile</Icon.PersonFill>)
-    return (
-        <Navbar variant="dark" expand="lg" >
-          <Navbar.Brand href="#home">AppTrack</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
+  const navDropDownTitle = (<Icon.Justify href="#menu" class="menu">Menu</Icon.Justify>)
+  const navHomeButton = (<Icon.HouseDoorFill href="#home" class="home">Home</Icon.HouseDoorFill>)
+  const navProfileButton = (<Icon.PersonFill href="#profile" class="profile">Profile</Icon.PersonFill>)
+  return (
 
-            <Nav className="ml-auto"> {/* align below items to the right */}
+    <Navbar variant="dark" expand="lg" >
+      <Container fluid>
+        <Navbar.Brand href="#home">AppTrack</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+
+          <Nav className="position-absolute end-0"> {/* align below items to the right */}
             <NavDropdown title={navDropDownTitle} id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">
-                  Dashboard</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">
+                Dashboard</NavDropdown.Item>
 
-                <NavDropdown.Item href="#action/3.2">
-                  Calendar</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Calendar</NavDropdown.Item>
 
-                <NavDropdown.Item href="#action/3.3">
-                  Companies</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Companies</NavDropdown.Item>
 
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Settings
-                </NavDropdown.Item>
-                
-              </NavDropdown>
-              
-              <button type="button" class="btn btn-link">{navProfileButton}</button>
-              <button type="button" class="btn btn-link">{navHomeButton}</button>
-            </Nav>
-          </Navbar.Collapse>
-      </Navbar>
-    );
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Settings
+              </NavDropdown.Item>
+
+            </NavDropdown>
+
+            <button type="button" class="btn btn-link">{navProfileButton}</button>
+            <button type="button" class="btn btn-link">{navHomeButton}</button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 export default getNavbar;
--- frontend/src/pages/Dashboard/Dashboard.jsx
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -3,88 +3,90 @@ import Navbar from "../../components/Navbar/Navbar"
 import './Dashboard.css';
 import '../../static/globals.css';
 
-let sampleNewUpdate = [ {"Company": "Meta", "Date":"1/1/2023", "Status":"Rejected"} ,
-                   {"Company": "Google", "Date":"1/1/2023", "Status":"Rejected"} ,
-                   {"Company": "Amazon", "Date":"1/1/2023", "Status":"Rejected"} ,
-                   {"Company": "Netflix", "Date":"1/1/2023", "Status":"Rejected"} ,
-                   {"Company": "Apple", "Date":"1/1/2023", "Status":"Rejected"} ,
-                   {"Company": "Walmart", "Date":"1/1/2023", "Status":"Rejected"} ,
-                   {"Company": "Mcdonalds", "Date":"1/1/2023", "Status":"Rejected"} ]
+let sampleNewUpdate = [{ "Company": "Meta", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Google", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Amazon", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Netflix", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Apple", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Walmart", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Mcdonalds", "Date": "1/1/2023", "Status": "Rejected" }]
 
-let sampleCompleted = [ {"Company": "Twilio", "Date":"1/1/2023", "Status":"Rejected"} ,
-                        {"Company": "Reddit", "Date":"1/1/2023", "Status":"Rejected"} ,
-                        {"Company": "Spotify", "Date":"1/1/2023", "Status":"Rejected"} ,
-                        {"Company": "Uber", "Date":"1/1/2023", "Status":"Rejected"} ,
-                        {"Company": "Pinterest", "Date":"1/1/2023", "Status":"Rejected"},
-                        {"Company": "Twitter", "Date":"1/1/2023", "Status":"Rejected"}]
+let sampleCompleted = [{ "Company": "Twilio", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Reddit", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Spotify", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Uber", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Pinterest", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Twitter", "Date": "1/1/2023", "Status": "Rejected" }]
 
 function Dashboard() {
 
-    const [newUpdateData, setNewUpdateData] = useState(sampleNewUpdate); // change in future 
-    const [completedData, setCompletedData] = useState(sampleCompleted); // change in the future
-    
-    const newUpdateTableRows = newUpdateData.map((info) => {
-        return (
-          <tr>
-            <td>{info.Company}</td>
-            <td>{info.Date}</td>
-            <td>{info.Status}</td>
-          </tr>
-        );
-      });
+  const [newUpdateData, setNewUpdateData] = useState(sampleNewUpdate); // change in future 
+  const [completedData, setCompletedData] = useState(sampleCompleted); // change in the future
 
-      const completedDataTableRows = completedData.map((info) => {
-        return (
-          <tr>
-            <td>{info.Company}</td>
-            <td>{info.Date}</td>
-            <td>{info.Status}</td>
-          </tr>
-        );
-      });
-
+  const newUpdateTableRows = newUpdateData.map((info) => {
     return (
-        <>
-            <Navbar />
-
-            {/* searchbar: https://bbbootstrap.com/snippets/bootstrap-5-search-bar-microphone-icon-inside-12725910*/}
-            <div class="search">
-                  <i class="fa fa-search"></i>
-                  <input type="text" class="form-control form-input" placeholder="Search your notifications"></input>
-            </div>
-            {/* fix this later */}
-
-            <div id="table-container">
-                <div id="new-updates-div" class="d-flex align-items-center">
-                    <div id="left-side-label-container">New Updates</div>
-                    <table class="table table-striped table-hover" id="new-update-table">  
-                        <thead>
-                        <tr>
-                            <th>Company</th>
-                            <th>Date</th>
-                            <th>Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>{newUpdateTableRows}</tbody>
-                    </table>
-                </div>
-                
-                <div id="completed-updates-div" class="d-flex align-items-center">
-                    <div id="left-side-label-container"><p>Completed</p></div>
-                    <table class="table table-striped table-hover" id="completed-table">  
-                        <thead>
-                        <tr>
-                            <th>Company</th>
-                            <th>Date</th>
-                            <th>Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>{completedDataTableRows}</tbody>
-                    </table>
-                </div>    
-            </div>
-        </>
+      <tr>
+        <td>{info.Company}</td>
+        <td>{info.Date}</td>
+        <td>{info.Status}</td>
+      </tr>
     );
+  });
+
+  const completedDataTableRows = completedData.map((info) => {
+    return (
+      <tr>
+        <td>{info.Company}</td>
+        <td>{info.Date}</td>
+        <td>{info.Status}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <>
+      <div className='text-center'>
+        <Navbar />
+
+        {/* searchbar: https://bbbootstrap.com/snippets/bootstrap-5-search-bar-microphone-icon-inside-12725910*/}
+        <div class="search">
+          <i class="fa fa-search"></i>
+          <input type="text" class="form-control form-input" placeholder="Search your notifications"></input>
+        </div>
+        {/* fix this later */}
+
+        <div id="table-container">
+          <div id="new-updates-div" class="d-flex align-items-center">
+            <div id="left-side-label-container">New Updates</div>
+            <table class="table table-striped table-hover" id="new-update-table">
+              <thead>
+                <tr>
+                  <th>Company</th>
+                  <th>Date</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>{newUpdateTableRows}</tbody>
+            </table>
+          </div>
+
+          <div id="completed-updates-div" class="d-flex align-items-center">
+            <div id="left-side-label-container"><p>Completed</p></div>
+            <table class="table table-striped table-hover" id="completed-table">
+              <thead>
+                <tr>
+                  <th>Company</th>
+                  <th>Date</th>
+                  <th>Status</th>
+                </tr>
+              </thead>
+              <tbody>{completedDataTableRows}</tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Dashboard;
