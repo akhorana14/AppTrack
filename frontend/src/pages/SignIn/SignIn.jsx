a0c39de8adbe9531c6d39ff0e0a0bc39db59645a
aarushisingh470 <aarushi.singh470@yahoo.com>
2023-02-12 15:13:39 -0500

    button styled

--- frontend/src/pages/SignIn/SignIn.jsx
+++ frontend/src/pages/SignIn/SignIn.jsx
@@ -10,9 +10,12 @@ function SignIn() {
             <div className="centersquare">
                 <h1>Welcome to AppTrack</h1>
                 <h2>You miss all the shots you don't take!</h2>
-                <div>
-                    <GoogleOAuthProvider clientId="350232392626-4lul15jckdjhqj2lkp4mphugoqbsvll7.apps.googleusercontent.com">
-                        <GoogleLogin onSuccess={handleLogin} onError={handleFailure} />
+                <div className="google">
+                    <GoogleOAuthProvider width="200px" clientId="350232392626-4lul15jckdjhqj2lkp4mphugoqbsvll7.apps.googleusercontent.com">
+                        <GoogleLogin
+                            onSuccess={handleLogin}
+                            onError={handleFailure}
+                        />
                     </GoogleOAuthProvider>
                 </div>
                 <p>Sorry! For now, we only support Google Authentication. More coming soon!</p>
