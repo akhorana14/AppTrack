5e85467ee1fcb83b5f32911effd41c10a0a0cc12
aarushisingh470 <aarushi.singh470@yahoo.com>
2023-02-17 05:32:14 -0500

    user story 11 done

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,11 +3,41 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import * as Icon from 'react-bootstrap-icons';
+import React from 'react';
+import { useState } from 'react';
 
 import './Navbar.css';
 
-function getNavbar() {
+let sampleNewUpdate = [{ "Company": "Meta", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Google", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Amazon", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Netflix", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Apple", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Walmart", "Date": "1/1/2023", "Status": "Rejected" },
+{ "Company": "Mcdonalds", "Date": "1/1/2023", "Status": "Rejected" }]
+
+function GetNavbar() {
+  const [visible, setVisible] = useState(true);
+
+  const removeElement = () => {
+    setVisible((prev) => !prev);
+  };
+
+  const newUpdateNotification = sampleNewUpdate.map((info) => {
+    return (
+      <NavDropdown.Item id={info.Company}>
+        {visible && (
+          <button className='buttonNotification'>
+            <p className='p1'>{info.Company}</p>
+            <p className='p2'>{info.Date}</p>
+          </button>
+        )}
+      </NavDropdown.Item>
+    )
+  });
+
   const navDropDownTitle = (<Icon.Justify href="#menu" class="menu">Menu</Icon.Justify>)
+  const navNotifications = (<Icon.BellFill href="#bell" class="bell">Notification</Icon.BellFill>)
   const navHomeButton = (<Icon.HouseDoorFill href="#home" class="home">Home</Icon.HouseDoorFill>)
   const navProfileButton = (<Icon.PersonFill href="#profile" class="profile">Profile</Icon.PersonFill>)
   return (
@@ -19,18 +49,26 @@ function getNavbar() {
         <Navbar.Collapse id="basic-navbar-nav">
 
           <Nav className="position-absolute end-0"> {/* align below items to the right */}
+            <NavDropdown title={navNotifications} id="notifications" align="end">
+              <div className='notificationHeading'>
+                <p className='headingNotification'>Notifications</p>
+                <button className="buttonRead" onClick={removeElement}>Mark all as read</button>
+              </div>
+              <NavDropdown.Divider />
+              <tbody>{newUpdateNotification}</tbody>
+            </NavDropdown>
             <NavDropdown title={navDropDownTitle} id="basic-nav-dropdown" align="end">
-              <NavDropdown.Item href="#action/3.1">
+              <NavDropdown.Item href="#action/4.1">
                 Dashboard</NavDropdown.Item>
 
-              <NavDropdown.Item href="#action/3.2">
+              <NavDropdown.Item href="#action/4.2">
                 Calendar</NavDropdown.Item>
 
-              <NavDropdown.Item href="#action/3.3">
+              <NavDropdown.Item href="#action/4.3">
                 Companies</NavDropdown.Item>
 
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item href="#action/4.4">
                 Settings
               </NavDropdown.Item>
 
@@ -44,4 +82,5 @@ function getNavbar() {
     </Navbar>
   );
 }
-export default getNavbar;
+
+export default GetNavbar;
