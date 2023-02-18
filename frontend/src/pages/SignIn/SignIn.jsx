f669bf6c03b703215e3ce88d180e4fe19c3f5787
IshaanRaja <ishaanrajapurdue@gmail.com>
2023-02-18 13:24:00 -0500

    Manually create Google identity component to request access to Gmail scope

--- frontend/src/pages/SignIn/SignIn.jsx
+++ frontend/src/pages/SignIn/SignIn.jsx
@@ -1,6 +1,5 @@
 import Navbar from "../../components/Navbar/Navbar"
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import { GoogleLogin } from '@react-oauth/google';
+import GoogleAuth from "../../components/GoogleAuth/GoogleAuth";
 import './SignIn.css';
 
 function SignIn() {
@@ -11,26 +10,12 @@ function SignIn() {
                 <h1>Welcome to AppTrack</h1>
                 <h2>You miss all the shots you don't take!</h2>
                 <div className="google">
-                    <GoogleOAuthProvider width="200px" clientId="350232392626-4lul15jckdjhqj2lkp4mphugoqbsvll7.apps.googleusercontent.com">
-                        <GoogleLogin
-                            onSuccess={handleLogin}
-                            onError={handleFailure}
-                        />
-                    </GoogleOAuthProvider>
+                    <GoogleAuth />
                 </div>
                 <p>Sorry! For now, we only support Google Authentication. More coming soon!</p>
             </div>
         </div>
     );
-};
-
-
-const handleFailure = (result) => {
-    alert(result);
-};
-
-const handleLogin = async (googleData) => {
-    console.log(googleData);
 };
 
 export default SignIn;
