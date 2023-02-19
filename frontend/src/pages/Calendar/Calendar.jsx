13b35383d15b9b5ce3ab979372298396ba5a64f9
Abhinav Dusi <dusi.abhinav2002@gmail.com>
2023-02-19 14:46:28 -0500

    Updated calendar

--- frontend/src/pages/Calendar/Calendar.jsx
+++ frontend/src/pages/Calendar/Calendar.jsx
@@ -151,8 +151,8 @@ function Calendar(props) {
                                 {
                                     upcomingPage > 1
                                     ? 
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-left-circle page-arrows" viewBox="0 0 16 16"  onClick={() => handleUpcomingPageChange(false)}>
-                                        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-arrow-left-circle page-arrows" viewBox="0 0 16 16"  onClick={() => handleUpcomingPageChange(false)}>
+                                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                                     </svg>
                                     :
                                     <></>
@@ -165,8 +165,8 @@ function Calendar(props) {
                                 {
                                     upcomingPage < numUpcomingPages
                                     ?
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-right-circle page-arrows" viewBox="0 0 16 16"  onClick={() => handleUpcomingPageChange(true)}>
-                                        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-right-circle page-arrows" viewBox="0 0 16 16" onClick={() => handleUpcomingPageChange(true)}>
+                                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                                     </svg>
                                     :
                                     <></>
@@ -182,8 +182,8 @@ function Calendar(props) {
                                 {
                                     completedPage > 1
                                     ? 
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-left-circle page-arrows" viewBox="0 0 16 16"  onClick={() => handleCompletedPageChange(false)}>
-                                        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="bi bi-arrow-left-circle page-arrows" viewBox="0 0 16 16"  onClick={() => handleCompletedPageChange(false)}>
+                                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
                                     </svg>
                                     :
                                     <></>
@@ -196,8 +196,8 @@ function Calendar(props) {
                                 {
                                     completedPage < numCompletedPages
                                     ?
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-right-circle page-arrows" viewBox="0 0 16 16"  onClick={() => handleCompletedPageChange(true)}>
-                                        <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-arrow-right-circle page-arrows" viewBox="0 0 16 16" onClick={() => handleCompletedPageChange(true)}>
+                                        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
                                     </svg>
                                     :
                                     <></>
