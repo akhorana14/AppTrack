37274d94a3bab7ed0a1da01295c079ee2432337f
aarushisingh470 <aarushi.singh470@yahoo.com>
2023-03-29 14:49:51 -0400

    tbody fixed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -54,7 +54,7 @@ function GetNavbar() {
                 <button className="buttonRead" onClick={() => removeElements()}>Clear All</button>
               </div>
               <NavDropdown.Divider />
-              <tbody>{companies.map((info, index) => (
+              <tbody className='tablebody'>{companies.map((info, index) => (
                 <NavDropdown.Item key={index} onClick={() => clickNotification(index)}>
                   <button className='buttonNotification' id={info.Company}>
                     <div className='buttonDiv'>
