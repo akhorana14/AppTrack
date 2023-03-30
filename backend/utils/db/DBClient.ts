99a22144934226f921b6aa8c7b773c3e8da699dd
rayyan-khan <rayyan.khan258@gmail.com>
2023-03-30 10:52:58 -0400

    edit

--- backend/utils/db/DBClient.ts
+++ backend/utils/db/DBClient.ts
@@ -9,7 +9,7 @@ const datasource = new DataSource({
     "port": 1433,
     "username": "apptrack",
     //Make sure you have added the database into your .env file
-    "password": "MwiCAm57^n1n",
+    "password": "",
     "database": "AppTrackDB",
     "synchronize": true,
     "logging": false,
