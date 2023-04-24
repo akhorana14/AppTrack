e7e446c5b3f86a763b841774949e225dd95a5631
akhorana14 <31749972+akhorana14@users.noreply.github.com>
2023-04-24 04:14:49 -0400

    frontend work for userstory 9

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import SignIn from "./pages/SignIn/SignIn"
 import Dashboard from "./pages/Dashboard/Dashboard"
 import Profile from "./pages/Profile/Profile"
 import Activate from "./pages/Activate/Activate"
+import Register from "./pages/Register/Register"
 import Navbar from "./components/Navbar/Navbar"
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -20,6 +21,7 @@ function App() {
             <Route path="/calendar" element={<Calendar />} />
             <Route path="/" element={<SignIn />} />
             <Route path="/profile" element={<Profile version="Account"/>} />
+            <Route path="/register" element={<Register version="Account"/>} />
             <Route path="/settings" element={<Profile version="Settings"/>} />
             <Route path="/company/:company" element={<Company />} />
             <Route path="/createApplication" element={<CreateApplication />} />
